6a98e38bb91f5773b319649deb6d9524a6798dc6
Anders Steen <anders.steen98@gmail.com name = Anders Steen>
2018-08-10 15:28:29 +0200

    Added touch controls to left and right parts of screen. Touch controls not verified, only tested with mouse click controls. Issue #23.

--- One Dim Dash/Assets/Scripts/PlayerControls.cs
+++ One Dim Dash/Assets/Scripts/PlayerControls.cs
@@ -7,15 +7,37 @@ public class PlayerControls : MonoBehaviour {
     public PlayerState playerState;
 
     public float accelerationMultiplier;
+    public float touchAccelerationMultiplier;
 
-	// Update is called once per frame
-	void Update () {
-        float horizontalInput = Input.GetAxis("Horizontal");
+    private Rect leftRect;
+    // private Rect rightRect; // really only a leftRect is needed
 
-        updateSpeed(horizontalInput);
-	}
+    void Start() {
+        leftRect = new Rect(0, 0, Screen.width / 2, Screen.height);
+        //rightRect = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
+    }
 
+    // Update is called once per frame
+    void Update () {
+        if (Input.touchCount > 0) {
+            int touchDirection = (leftRect.Contains(Input.GetTouch(0).position)) ? -1 : 1; // Determines whether the left or right side of the screen is being touched, and sets the direction of acceleration
+            updateSpeed(touchDirection);
+        } else if (Input.GetMouseButton(0)) {
+            int touchDirection = (leftRect.Contains(Input.mousePosition)) ? -1 : 1;
+            updateSpeed(touchDirection);
+        }
+
+        // Deprecated
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //updateSpeed(horizontalInput);
+    }
+
+    // Deprecated
     private void updateSpeed(float horizontalInput) {
         playerState.speed = playerState.speed + horizontalInput * Time.deltaTime * accelerationMultiplier;
+    }
+
+    private void updateSpeed(int touchDirection) {
+        playerState.speed = playerState.speed + touchDirection * Time.deltaTime * touchAccelerationMultiplier;
     }
 }
